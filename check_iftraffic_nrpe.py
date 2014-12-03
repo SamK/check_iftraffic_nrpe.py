e41830baa77fd8bc490dab4800ae237f305cca57
Samuel Krieg <samuel.krieg+github@gmail.com>
2014-12-03 14:11:31 +0100

    use NagiosService.status() instead of non existent property. Fixes #27

--- check_iftraffic_nrpe.py
+++ check_iftraffic_nrpe.py
@@ -260,7 +260,7 @@ class NagiosService(object):
                 'min_level': self.min_level,
                 'max_level': self.max_level}
 
-    def status(self, value):
+    def status(self):
         """Returns the string defining the Nagios status of the value"""
         if self.value >= self.crit_level:
             return 'CRITICAL'
@@ -297,6 +297,7 @@ class NagiosResult(object):
         """Return the output of a Nagios check"""
 
         # The name and the status
+
         output = "%s %s" % (self.name, self.status)
 
         # Some messages if existing
@@ -325,7 +326,7 @@ class NagiosResult(object):
     def add(self, new_service):
         """ Add a NagiosService object in the Nagios results
         """
-        self.status = self.worst(self.status, new_service.status)
+        self.status = self.worst(self.status, new_service.status())
         self._services.append(new_service)
 
 
