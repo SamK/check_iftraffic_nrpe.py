SamK/check_iftraffic_nrpe.py

commit 3e273449a1d0f47c63fb347e3ece6da364ccf729

add units in documentation and in perfdata output. Fixes #18

check_iftraffic_nrpe.py
@@ -344,7 +344,6 @@ def parse_arguments(default_values):
 
     return p.parse_args()
 
-
 def main(default_values):
     """This main function is wayyyy too long"""
 
@@ -469,22 +468,22 @@ def main(default_values):
             for counter in default_values['counters']:
 
                 # calculate the bytes
-                units = calc_diff(if_data0[if_name][counter['name']], uptime0,
+                value = calc_diff(if_data0[if_name][counter['name']], uptime0,
                                   if_data1[counter['name']], uptime1)
 
                 # calculate the bytes per second
-                units /= elapsed_time
+                value /= elapsed_time
 
                 #
                 # Decide a Nagios status
                 #
 
-                new_exit_status = nagios_value_status(units, args.bandwidth,
+                new_exit_status = nagios_value_status(value, args.bandwidth,
                                                       args.critical,
                                                       args.warning)
                 if new_exit_status != 'OK':
                     problems.append("%s: %s/%s" %
-                                    (if_name, units, args.bandwidth))
+                                    (if_name, value, args.bandwidth))
                 exit_status = worst_status(exit_status, new_exit_status)
 
                 #
@@ -496,11 +495,10 @@ def main(default_values):
                 (user_readable_message_for_nagios) | (label)=(value)(metric);
                 (warn level);(crit level);(min level);(max level)
                 """
-
-                output_value = "%.2f" % units
+                output_value = "%.2fc" % value
                 warn_level = str(int(args.warning) * (args.bandwidth / 100))
                 crit_level = str(int(args.critical) * (args.bandwidth / 100))
-                min_level = str(0.0)
+                min_level = '0'
                 max_level = str(args.bandwidth)
 
                 perfdata.append(format_perfdata(counter['prefix'] + if_name,

commit 4d08b569a48bb59513cb4917c08985f264b518f6

Remove the "c" UOM of perfdata output. Closes #30.
The issue has been introduced in 3e27344 by issue #18.

The "c" thing means it's a continous counter (a number increasing over
time). However the output of check_iftraffic_nrpe.py is not a counter
but a value of bytes per second (by default).

The Nagios plugins guideline does not specify a suitable unit but "no
unit specified".

check_iftraffic_nrpe.py
@@ -250,7 +250,7 @@ class NagiosService(object):
 
     def __str__(self):
         """Return the perfdata string"""
-        return '%(label)s=%(value).2fc;' \
+        return '%(label)s=%(value).2f;' \
                '%(warn_level)s;' \
                '%(crit_level)s;' \
                '%(min_level)s;' \